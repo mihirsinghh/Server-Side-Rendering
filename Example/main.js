41a61ec579b7f6a9b5e8eef8a40c45dbee932f15
Mihir <mihirsingh@berkeley.edu>
2024-10-26 00:31:25 -0700

    added comments to templating code

--- Example/main.js
+++ Example/main.js
@@ -8,7 +8,7 @@ let totalViews = 0;
 
 app.get('/', (req, res) => {
     totalViews++;
-    let html = fs.readFileSync(__dirname + '/index.html', 'utf8'); //reads the HTML file from the same directory as the JS file
+    let html = fs.readFileSync(__dirname + '/index.html', 'utf8'); //reads the HTML file (index.html) from the same directory as the JS file
     html = html.replace("{{views}}", totalViews); //replaces the "views" placeholder with the dynamic totalViews value
     res.send(html); //sends the modified HTML back to the user's browser
 });
